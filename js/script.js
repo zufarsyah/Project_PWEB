3278f44aa22bba39417fa69f518e6aa560801970
Dapuluous <mdhaffamahendra@gmail.com>
2020-11-13 22:35:06 +0700

    Added more pages and adjustment to interface

--- js/script.js
+++ js/script.js
@@ -4,9 +4,24 @@ $(document).ready(function(){
 		$(".nav-content").toggle(500);
 	});
 
+	$(".mobileShowDropdown").click(function() {
+		$(".dropdown-content").toggle(500);
+	});
 	// ===== Scroll to Top on click =====
 	$(".arrow-button").click(function(){
 		$("html, body").animate({ scrollTop: "0" }, 300); 
+	});
+
+	// ===== Price live update =====
+	$(document).ready(function(){
+		$('#quantity').change(function(){
+			if($('#quantity').val() < 0) {
+				$('#totalPrice').html("Rp 0");
+			} else {
+				var totalHarga = $('#productPrice').val() * this.value;
+		    	$('#totalPrice').html("Rp " + totalHarga);
+			}
+		});
 	});
 });
 
